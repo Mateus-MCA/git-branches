772dcc31d102a15b7cde9f974048802bb3f75ae4
Brenosilva13 <breno.souza@germinare.org.br>
2024-03-13 11:57:21 -0300

    corrigindo merge

--- Cliente.java
+++ Cliente.java
@@ -1,12 +1,7 @@
 public class Cliente {
-<<<<<<< HEAD
-}
-gggg
-=======
     private String tipoConta;
     private String nome;
     private String cpf;
     private int agencia;
     private double saldo;
 }
->>>>>>> 6276b231ef69fd1efbbe2186e4d68ec760eeb7a9
